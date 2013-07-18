fd543b8571a0596d18627d7fac34a1c9f38b04bf
Jack Valmadre <jack.valmadre@gmail.com>
2013-07-18 10:01:56 +1000

    Improved documentation

--- task.go
+++ task.go
@@ -2,25 +2,30 @@ package grideng
 
 import "io"
 
+// An input knows its name and how to serialize itself.
 type Input interface {
-	// Uniquely defines an input.
+	// Uniquely identifies an input.
 	Name() string
 	Write(io.Writer) error
 }
 
+// A task has an input and a method of execution.
 type Task interface {
 	Input() Input
 	Execute() (Output, error)
 }
 
+// An output knows how to serialize itself.
 type Output interface {
 	Write(io.Writer) error
 }
 
+// Deserializes an input and returns a task which is given it.
 type InputReader interface {
 	Read(io.Reader) (Task, error)
 }
 
+// Deserializes an output.
 type OutputReader interface {
 	Read(io.Reader) (Output, error)
 }
